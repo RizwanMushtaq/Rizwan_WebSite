46e9b715d56b20b948263c99618c5e805b10e5cc
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-04-25 13:59:54 +0200

    first time adding all files from local

---
+++ src/scrollAcrossPage.js
@@ -0,0 +1,32 @@
+function scrollToAboutMe() {
+    const aboutMe = document.getElementById('aboutMe');
+    aboutMe.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function scrollToSkills() {
+    const skills = document.getElementById('skills');
+    skills.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function scrollToPortfolio() {
+    const portfolio = document.getElementById('portfolio');
+    portfolio.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
+function downloadCV() {
+    console.log('Function under development');
+    alert('Function under development');
+}
+function scrollToContact() {
+    const contact = document.getElementById('contact');
+    contact.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+    });
+}
